165d235422dd197f98545ca99d1db8e5ec82c59a
jakubpasieka55 <177260@stud.prz.edu.pl>
2025-05-20 14:45:14 +0000

    42

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         String input = "Hello, world!";
-        int index = 7; // Example index
-        char character = input.charAt(index);
-        System.out.println(character);
+        char toRemove = 'o';
+        String result = input.replace(String.valueOf(toRemove), "");
+        System.out.println(result);
     }
 }
