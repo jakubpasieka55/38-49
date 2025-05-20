24aba347b395a804ca16108784438f50751eedb8
jakubpasieka55 <177260@stud.prz.edu.pl>
2025-05-20 14:53:25 +0000

    48

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 1, b = 0, c = -1; // Example numbers
-        if (a < b && b < c) {
-            System.out.println("Increasing");
-        } else if (a > b && b > c) {
-            System.out.println("Decreasing");
-        } else {
-            System.out.println("Neither");
-        }
+        String input = "hello world!";
+        String uppercased = input.toUpperCase();
+        System.out.println(uppercased);
     }
 }
