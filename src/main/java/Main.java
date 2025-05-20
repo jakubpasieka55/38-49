9bd06ab77449c9e4c5f6c3290fb61e312d667335
jakubpasieka55 <177260@stud.prz.edu.pl>
2025-05-20 14:44:14 +0000

    41

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        String input = "Hello, dear dog!";
-        String replaced = input.replace('d', 'f');
-        System.out.println(replaced);
+        String input = "Hello, world!";
+        int index = 7; // Example index
+        char character = input.charAt(index);
+        System.out.println(character);
     }
 }
