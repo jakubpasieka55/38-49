9c49f284dbbf24cdcda3b0e462361385fc55f10f
jakubpasieka55 <177260@stud.prz.edu.pl>
2025-05-20 14:50:57 +0000

    46

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int number = -5; // Example number
-        if (number > 0) {
-            System.out.println("Positive");
-        } else if (number < 0) {
-            System.out.println("Negative");
+        int a = 3, b = 3, c = 4; // Example numbers
+        if (a == b && b == c) {
+            System.out.println("All three numbers are equal.");
         } else {
-            System.out.println("Zero");
+            System.out.println("Not all numbers are equal.");
         }
     }
 }
