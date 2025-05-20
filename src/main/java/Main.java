8cc561328a93e8835c25652611694530b42d9e7e
jakubpasieka55 <177260@stud.prz.edu.pl>
2025-05-20 14:50:07 +0000

    44

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,12 @@
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a positive integer: ");
-        int number = scanner.nextInt();
-        int count = String.valueOf(number).length();
-        System.out.println("Number of digits: " + count);
+        int number = -5; // Example number
+        if (number > 0) {
+            System.out.println("Positive");
+        } else if (number < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
     }
 }
